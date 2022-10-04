15ee4a10c6ba6627f38515fe82a153cb275deb31
Zachary Grider <zag0003@auburn.edu>
2022-10-04 15:29:57 -0500

    built websocket listener to sensor inbox

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 const { Session } = require("@inrupt/solid-client-authn-node");
 const { WebsocketNotification } = require('@inrupt/solid-client-notifications');
 require('dotenv').config();
-const { saveSolidDatasetAt, 
+const { 
+  saveSolidDatasetAt, 
   buildThing, 
   saveSolidDatasetInContainer, 
   createContainerInContainer, 
   getSolidDataset, 
-  createThing, 
+  createThing,
+  getThing,
   setThing,
+  getUrl,
+  getStringNoLocale,
   createSolidDataset } = require('@inrupt/solid-client');
 const session = new Session();
 session.login({
@@ -17,5 +21,41 @@ session.login({
 }).then(async () => {
     const webId = session.info.webId;
     const data = await getSolidDataset(webId, {fetch: session.fetch});
-    console.log(data) 
+    const graph = getThing(data, webId)
+    const storage = getUrl(graph, 'http://www.w3.org/ns/pim/space#storage');
+    const extendedProfileUri = getUrl(graph, 'http://www.w3.org/2000/01/rdf-schema#seeAlso');
+    //console.log(storage);
+    //console.log(extendedProfileUri);
+    const extendedProfile = await getSolidDataset(extendedProfileUri, { fetch: session.fetch });
+    //console.log(extendedProfile);
+    const extendedProfileWebIdThing = getThing(extendedProfile, webId);
+    // console.log(extendedProfileWebIdThing)
+    const sensorInboxUri = getStringNoLocale(extendedProfileWebIdThing, 'http://www.example.org/sensor#sensorInbox')
+    //console.log(sensorInboxUri);
+    const fullSensorUri = `${storage}${sensorInboxUri}`;
+    //const sensorData = await getSolidDataset(fullSensorUri, { fetch: session.fetch });
+    //console.log(sensorData);
+    if (fullSensorUri) {
+        const ws = new WebsocketNotification(
+            fullSensorUri,
+            { fetch: session.fetch }
+        )
+    
+        ws.on("error", (error) => {
+            console.log(error);
+        })
+    
+        ws.on("connected", () => {
+            console.log('connected!')
+        })
+    
+        ws.on("closed", () => {
+            console.log('closed!')
+        });
+    
+        ws.on("message", (notif) => {
+            console.log(notif);
+        })
+    }
+    
 }).catch((err) => console.log(err));
