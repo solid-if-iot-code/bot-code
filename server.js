4aad9643f9ba68ce48ca61652dcf699211f3d7d3
Zachary Grider <zag0003@auburn.edu>
2022-10-06 16:37:38 -0500

    added stuff to get new things and listen to sensor inbox container and then hook websocket onto it

--- server.js
+++ server.js
@@ -12,7 +12,8 @@ const {
   setThing,
   getUrl,
   getStringNoLocale,
-  createSolidDataset } = require('@inrupt/solid-client');
+  createSolidDataset,
+getThingAll } = require('@inrupt/solid-client');
 const mqtt = require('mqtt');
 
 const session = new Session();
@@ -29,17 +30,22 @@ session.login({
     const webId = session.info.webId;
     const data = await getSolidDataset(webId, {fetch: session.fetch});
     const graph = getThing(data, webId);
-    const storage = getUrl(graph, 'http://www.w3.org/ns/pim/space#storage');
+    //const storage = getUrl(graph, 'http://www.w3.org/ns/pim/space#storage');
     const extendedProfileUri = getUrl(graph, 'http://www.w3.org/2000/01/rdf-schema#seeAlso');
     const extendedProfile = await getSolidDataset(extendedProfileUri, { fetch: session.fetch });
     const extendedProfileWebIdThing = getThing(extendedProfile, webId);
     const sensorInboxUri = getStringNoLocale(extendedProfileWebIdThing, 'http://www.example.org/sensor#sensorInbox');
-    const fullSensorUri = `${storage}${sensorInboxUri}`;
+    console.log(sensorInboxUri);
+    //const fullSensorUri = `${storage}${sensorInboxUri}`;
     //const sensorData = await getSolidDataset(fullSensorUri, { fetch: session.fetch });
     //console.log(sensorData);
-    /**if (fullSensorUri) {
+    let cache = [];
+    let socketListeners = {};
+    if (sensorInboxUri) {
+        let dataset = await getSolidDataset(sensorInboxUri, { fetch: session.fetch });
+        console.log(sensorInboxUri)
         const ws = new WebsocketNotification(
-            fullSensorUri,
+            sensorInboxUri,
             { fetch: session.fetch }
         )
     
@@ -49,17 +55,54 @@ session.login({
     
         ws.on("connected", () => {
             console.log('connected!')
+            cache = getThingAll(dataset);
+            cache = cache.map(thing => thing.url)
+            console.log(cache);
         })
     
         ws.on("closed", () => {
             console.log('closed!')
         });
     
-        ws.on("message", (notif) => {
+        ws.on("message", async (notif) => {
             console.log(notif);
+            dataset = await getSolidDataset(sensorInboxUri, { fetch: session.fetch})
+            let newThings = getThingAll(dataset);
+            newThings = newThings.map(thing => thing.url);
+            //console.log(cache)
+            //console.log(newThings)
+            let newThing = newThings.filter(url => !cache.includes(url));
+            cache = newThings;
+            const newUrl = newThing[0];
+            const wss = new WebsocketNotification(
+                newUrl,
+                { fetch: session.fetch }
+            )
+        
+            wss.on("error", (error) => {
+                console.log(error);
+            })
+        
+            wss.on("connected", () => {
+                console.log(`connected to ${newUrl}!`)
+            })
+        
+            wss.on("closed", () => {
+                console.log('closed!')
+            });
+        
+            wss.on("message", (notif) => {
+                console.log(notif);
+            })
+
+            wss.connect();
+            socketListeners[newUrl] = wss;
+            console.log(socketListeners)
         })
-    }*/
-    const id = generateRandomId();
+
+        ws.connect();
+    }
+    /**const id = generateRandomId();
     const url = 'mqtt://broker.hivemq.com/'
     const publishTopic = 'uark/csce5013/test'
     const subscribeTopic = 'uark/csce5013/ahnelson/light'
@@ -87,5 +130,11 @@ session.login({
     const msg = '79'
     client.publish(publishTopic, msg, () => {
         console.log(`published ${msg}`)
-    })
+    })**/
 }).catch((err) => console.log(err));
+
+function createWebsocket(session, url) {
+    
+
+    return ws;
+}
